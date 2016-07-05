b7877f04512771b0d49a13600cd5321a1ab64795
Megan Fanning <megantfanning@gmail.com>
2016-07-05 14:34:37 -0700

    basic server and client

--- client.py
+++ client.py
@@ -1,9 +1,78 @@
-#determine if verticalClient or horizontalClient
+#!/usr/bin/python2.7
+# references
+# https://wiki.python.org/moin/TcpCommunication 
+import sys
+import socket
 
-#connect to server
+##-- Functions --##
+#get the user's "handle" by initial query
+# handle is a one-word name, up to 10 characters. 
+def getUsersHandle():
+    print("What would you like to your handle, it must be 10 characters or less")
 
-#allow user to input text
+    person = raw_input('Enter your name: ')
+    #check if handle is less than 10 char 
+    if(len(person) > 10):
+        print("error! requested handle is too long")
+        person=getUsersHandle();
 
-#check if text is a direction
+    #check if there are spaces in handle
+    if(" " in person): 
+        print("There's a space in your username")
+        person=getUsersHandle();
+    return person
+
+def sendMsg(input,usersHandle):
+    #prepend users handle and append newline
+    return usersHandle+">"+input+"\n" 
+
+#should be able to get hostname, messages etc    
+def getUserInput(user):
+    userInput = raw_input(user+'>')
+    return userInput
+
+
+##-- MAIN --##
+def main():
+    #specify host A's hostname and port number on the command line.
+    TCP_IP = "localhost" #sys.argv[1] TODO temp fix
+    TCP_PORT = 9999 #int(sys.argv[2])  
+    BUFFER_SIZE = 1024
+    quit="\quit"
+
+    #connect to server
+    #create sock stream and connect  
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+
+    #get user Handle
+    userHandle = getUsersHandle()
+    
+    #TODO determine if verticalClient or horizontalClient
+
+    #get initial input
+    msg = getUserInput(userHandle)
+    MESSAGE = sendMsg(msg,userHandle)
+
+    #while the user hasn't quit send and receive msgs
+    while(msg != quit):
+        #send message
+        s.send(MESSAGE.encode())
+        data = s.recv(BUFFER_SIZE).decode()
+        sys.stdout.write(data)
+
+        #allow user to input text
+        msg = getUserInput(userHandle)
+        # TODO check if text is a direction
+
+        MESSAGE = sendMsg(msg,userHandle)
+    #close connection
+    s.close()
+
+if __name__ == "__main__":
+    main()#TODO add back argv to allow random ports
+
+
+
 
 
--- server.py
+++ server.py
@@ -1,6 +1,36 @@
-#Python Server
+#!/usr/bin/python2.7
+import SocketServer
+#Server
+#references : https://docs.python.org/2/library/socketserver.html
 
-#call function to process user movement
+class MyTCPHandler(SocketServer.BaseRequestHandler):
+    """
+    The request handler class for our server.
 
-#call function to pass user text back to other client
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print "{} wrote:".format(self.client_address[0])
+        print self.data
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+        #TODO call function to process user movement
+        #TODO call function to pass user text back to other client
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999 #TODO remove magic # 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
 
