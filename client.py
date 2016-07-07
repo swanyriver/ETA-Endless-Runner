800703d7fa785d92678017aafdc851ae66b9f952
Megan Fanning <megantfanning@gmail.com>
2016-07-07 00:02:00 -0700

    fixed server to allow sustained connection

--- client.py
+++ client.py
@@ -5,32 +5,11 @@ import sys
 import socket
 
 ##-- Functions --##
-#get the user's "handle" by initial query
-# handle is a one-word name, up to 10 characters. 
-def getUsersHandle():
-    print("What would you like to your handle, it must be 10 characters or less")
 
-    person = raw_input('Enter your name: ')
-    #check if handle is less than 10 char 
-    if(len(person) > 10):
-        print("error! requested handle is too long")
-        person=getUsersHandle();
-
-    #check if there are spaces in handle
-    if(" " in person): 
-        print("There's a space in your username")
-        person=getUsersHandle();
-    return person
-
-def sendMsg(input,usersHandle):
-    #prepend users handle and append newline
-    return usersHandle+">"+input+"\n" 
-
-#should be able to get hostname, messages etc    
+#user is either vertical or horizontal  
 def getUserInput(user):
     userInput = raw_input(user+'>')
     return userInput
-
 
 ##-- MAIN --##
 def main():
@@ -39,40 +18,34 @@ def main():
     TCP_PORT = 9999 #int(sys.argv[2])  
     BUFFER_SIZE = 1024
     quit="\quit"
+    userHandle="vertical" #TODO call function to find out which user this is
 
     #connect to server
     #create sock stream and connect  
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-
-    #get user Handle
-    userHandle = getUsersHandle()
     
     #TODO determine if verticalClient or horizontalClient
 
     #get initial input
-    msg = getUserInput(userHandle)
-    MESSAGE = sendMsg(msg,userHandle)
+    MESSAGE = getUserInput(userHandle)
 
     #while the user hasn't quit send and receive msgs
-    while(msg != quit):
+    while(MESSAGE != quit):
         #send message
         s.send(MESSAGE.encode())
         data = s.recv(BUFFER_SIZE).decode()
         sys.stdout.write(data)
 
         #allow user to input text
-        msg = getUserInput(userHandle)
+        MESSAGE = getUserInput(userHandle)
         # TODO check if text is a direction
 
-        MESSAGE = sendMsg(msg,userHandle)
+    print("closing")
+
     #close connection
     s.close()
 
 if __name__ == "__main__":
     main()#TODO add back argv to allow random ports
-
-
-
-
 
--- server.py
+++ server.py
@@ -6,21 +6,26 @@ import SocketServer
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     """
     The request handler class for our server.
-
     It is instantiated once per connection to the server, and must
     override the handle() method to implement communication to the
     client.
     """
 
     def handle(self):
+        serverActive=True
         # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
-        #TODO call function to process user movement
-        #TODO call function to pass user text back to other client
+        while serverActive:
+            received=self.request.recv(1024)
+            if received==0:
+                serverActive=False
+            else:
+                self.data = received.strip()
+                print "{} wrote:".format(self.client_address[0])
+                print self.data
+                # just send back the same data, but upper-cased
+                self.request.sendall(self.data.upper())
+                #TODO call function to process user movement
+                #TODO call function to pass user text back to other client
 
 
 if __name__ == "__main__":
