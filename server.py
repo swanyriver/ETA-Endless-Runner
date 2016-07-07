74c98cb0013c6fad306fa8dd9d0347830e87ab8e
Megan Fanning <megantfanning@gmail.com>
2016-07-07 12:51:00 -0700

    fixed newline on server data print back to client

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
                 print "{} wrote:".format(self.client_address[0])
                 print self.data
                 # just send back the same data, but upper-cased
-                self.request.sendall(self.data.upper())
+                self.request.sendall(self.data.upper()+"\n")
                 #TODO call function to process user movement
                 #TODO call function to pass user text back to other client
 
