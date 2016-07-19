8904f70cf12808829632d944e0817d4a02ba0c85
Brandon Swanson <swanyriver@gmail.com>
2016-07-19 16:54:47 -0700

    uniform sized drawing array with padding removed

--- GraphicMaker/main.py
+++ GraphicMaker/main.py
@@ -17,11 +17,43 @@
 import webapp2
 import json
 
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         with open('generator.html', 'r') as outputFile:
             self.response.write(outputFile.read())
             return
+
+    @staticmethod
+    def uniformDrawing(drawing):
+        # NOTE: this algorithm creates a drawing of uniformly lengthed strings by
+        # removing all right padding
+        # remove all-whitespace lines at beginning and end of drawing
+        # then add back right padding up to the longest(s)(non-whitespace-to-not-whitespace) strings in the drawing
+        # remove unnecessary left padding (at least one string will have a non whitespace char in index 0)
+
+        print "entering drawing method:", drawing
+
+        drawing = [line.rstrip() for line in drawing]
+
+        while drawing and not drawing[0]:
+            del drawing[0]
+        while drawing and not drawing[-1]:
+            del drawing[-1]
+
+        # return early if all lines were whitespace
+        if not drawing: return drawing
+
+        maxLength = max(len(line) for line in drawing)
+        drawing = [line + (" " * (maxLength - len(line))) for line in drawing]
+
+        leftRemove = 0
+        while leftRemove < min(len(line) for line in drawing):
+            if not all(line[leftRemove] == " " for line in drawing): break
+            leftRemove += 1
+        drawing = [line[leftRemove:] for line in drawing]
+
+        return drawing
 
     # create graphics JSON from form data
     def post(self):
@@ -31,12 +63,16 @@ class MainHandler(webapp2.RequestHandler):
         outputDict = {}
         if kDEADLY in self.request.POST:
             outputDict[kDEADLY] = True
-        drawings = [d.splitlines() for d in self.request.POST.getall(kDRAWING) if d]
+
+        #uniform pad nonEmpty frames
+        drawings = [MainHandler.uniformDrawing(d.splitlines()) for d in self.request.POST.getall(kDRAWING) if d]
+        #remove empty frames
+        drawings = filter(None, drawings)
+
 
         #todo render json in html with option to return to editing including extra frames
         #todo run parse tests on server and display error/success
         #todo show game rendering of graphic  (is this even possible??)
-        #todo remove excess left padding,  add right padding
         if drawings:
             outputDict[kDRAWING] = drawings
             self.response.headers['Content-Type'] = "application/json"
