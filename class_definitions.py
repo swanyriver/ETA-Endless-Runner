370f7f2ffc766337c92a31c41694712876fdd6c5
weldonmi <weldonmi@onid.oregonstate.edu>
2016-07-19 17:32:44 -0700

    Classes updated for json, still needs threading

--- class_definitions.py
+++ class_definitions.py
@@ -1,203 +1,158 @@
 import time
+import json
 
-#NOTES:
-#store location (player)
-#client sends awsd and server does processing
-#UDP-style client coordinating
+#player class: initializes location to outside the grid
+class Player():
+	#takes up one space, which shouldn't automatically be -1, -1;
+	#invalid location until set for gameplay
+	def __init__(self, name):
+		self.name = name
+		self.x = -1
+		self.y = -1
 
-#Functions to Use:
-#game = Gamestate()
-#game.startTimer()
-#game.stopTimer()
-#game.gameTime is the total elapsed time
-#             still determining score..
-#game.newPlayer. movePlayerUp(playableGrid, newPlayer) syntax?
-#game.newPlayer. movePlayerDown(playableGrid, newPlayer)
-#game.newPlayer. movePlayerLeft(playableGrid, newPlayer)
-#game.newPlayer. movePlayerRight(playableGrid, newPlayer)
-#
+#	def get_location(self):
+#		return y, x
 
-#gamestate class
-#set up gamestate and keeps track of user scores and grid
-class Gamestate():
-                t0, t1
+	#functions for single-space movement
+	def _move_up(self):
+		self.y = self.y - 1
+	def _move_down(self):
+		self.y = self.y + 1
+	def _move_left(self):
+		self.x = self.x - 1
+	def _move_right(self):
+		self.x = self.x + 1
 
-                #sets up initial variables and grid
-                def __init__(self):
-                                newGrid = Grid()
-                                roomsCrossed = 0
-                                score = 0
-                                gameTime = 0
-                                newPlayer = Player()
- 
-                                #add obstacles initially?
+	#functions to set initial player location in room
+	def set_x(self, new_x):
+		self.x = new_x
+	def set_y(self, new_y):
+		self.y = new_y
 
-                                #adds player to middle of grid
-                                #Player._set_location(newPlayer, newGrid.midY, newGrid.midX)
+	#function to remove player from board (if needed)
+	#doesn't actually get rid of object but moves player
+	#outside of grid, makes player unusable
+	def remove_player(self):
+		self.x = -1
+		self.y = -1
 
-                #add timer
-                def startTimer(self):
-                                t0 = time.time()
+	#sends a json message to _________
+	#IS THIS RIGHT??
+	def update(self):
+		data = {'name': self.name, 'x': self.x, 'y': self.y}
+		json.dumps(data)
+		return
 
-                #end timer
-                def endTimer(self):
-                                t1 = time.time()
-                                gameTime = t1 – t0
+	#recieves a json message from ______
+	def get_change_request(self, m)
+		req = json.loads(m)
+		#do something here..
+		return
 
-                #keep track of rooms crossed
+#obstacle class: initializes location to outside the grid
+class Obstacle():
+	#takes up one space, which shouldn't automatically be -1, -1;
+	#invalid location until set for gameplay
+	def __init__(self, name):
+		self.name = name
+		self.x = -1
+		self.y = -1
+
+#	def get_location(self):
+#		return y, x
+
+	#functions to set initial obstacle location in room
+	def set_x(self, new_x):
+		self.x = new_x
+	def set_y(self, new_y):
+		self.y = new_y
+
+	#function to remove obstacle from board (if needed)
+	#doesn't actually get rid of object but moves obstacle
+	#outside of grid, makes it unusable
+	def remove_obstacle(self):
+		self.x = -1
+		self.y = -1
+
+	#sends a json message to _________
+	#IS THIS RIGHT??
+	def update(self):
+		data = {'name': self.name, 'x': self.x, 'y': self.y}
+		json.dumps(data)
+		return
+
+	#recieves a json message from ______
+	def get_change_request(self, m)
+		req = json.loads(m)
+		#do something here..
+		return
 
 #grid class
-Class Grid():
-	_width = 80
-        _height = 24
+class Grid():
+	#initilized to default values for screen size
+	#definitely can be changed with no apparent issue
+	width = 80
+        height = 24
 
-        #check this!
-        midY = _height/2
-        midX = _width/2
+	#function to set grid width and height
+	def set_width(self, new_w):
+		self.width = new_w
+	def set_height(self, new_h):
+		self.height = new_h
 
-        temp_location = [0 for in range(2)]
+	#sends a json message to _________
+	#IS THIS RIGHT??
+	def update(self):
+		data = {'width': self.width, 'height': self.height}
+		json.dumps(data)
+		return
 
-        #1 for walls, 2 for obstacles, 3 for player, 0 for free space
-        #returns nothing, walls are 1 space thick
-        def __init__(self):
-		playableGrid = [[1 for x in range(_width)] for y in range(_height)]
-        	#playableGrid = [[0 for x in range(1, _width)] for y in range(1, _height)]
-                #create walls
-		for x in range(1, _width):
-			for y in range(1, _height):
-                               	playableGrid[y][x] = 0 
+	#recieves a json message from ______
+	def get_change_request(self, m)
+		req = json.loads(m)
+		#do something here..
+		return
 
-        #create doors
-        ##
+#gamestate class: set up gamestate and keeps track of user scores
+#and grid. I don't believe the Gamestate class itself stores instances
+#of any other class
+class Gamestate():
+	t0, t1, gameTime, roomsCrossed, gameOver = 0
 
-        #random generation
-        ##
+	#sets up initial variables and grid
+	def __init__(self):
+		newGrid = Grid()
+		roomsCrossed = 0
+		score = 0
+		gameTime = 0
+		newPlayer = Player()
 
-        #returns updated playable grid space
-        def addObstable(self, y, x ):
-	        playableGrid[y][x] = 2
-                return playableGrid
+		#add obstacles initially?
 
-        #returns updated playable grid space
-        def removeObstable(self, obstacle, y, x):
-                Obstacle._set_location(obstacle) = {-1, -1}
-                playableGrid[y][x] = 0
-                return playableGrid
+		#adds player to middle of grid
+		#Player._set_location(newPlayer, newGrid.midY, newGrid.midX)
 
-        #returns updated playable grid space
-        def addPlayer(self, player, y, x):
-        	Player._set_location(player, y, x)
-                playableGrid[y][x] = 3
-                return playableGrid
+	#add timer
+	def startTimer(self):
+		self.t0 = time.time()
+	#end timer
+	def endTimer(self):
+		self.t1 = time.time()
+		self.gameTime = self.t1 – self.t0
 
-        #returns updated playable grid space
-        def removePlayer(self, player, y, x):
-        	Player._set_location(player) = {-1, -1}
-                playableGrid[y][x] = 0
-                return playableGrid
-
-        #move player functions, includes cleanup but
-        #assumes there is going to be empty space behind the player
-        def movePlayerUp(self, player):
-	        temp_location = Player._get_location(player)
-	        playableGrid[temp_location[0]][temp_location[1]] = 0
-	        Player._move_up(player)
-	        playableGrid[temp_location[0]][temp_location[1]] = 3
-
-        def movePlayerDown(self, player):
-        	temp_location = Player._get_location(player)
-        	playableGrid[temp_location[0]][temp_location[1]] = 0
-        	Player._move_down(player)
-        	playableGrid[temp_location[0]][temp_location[1]] = 3
-
-        def movePlayerLeft(self, player):
-	        temp_location = Player._get_location(player)
-	        playableGrid[temp_location[0]][temp_location[1]] = 0
-	        Player._move_left(player)
-	        playableGrid[temp_location[0]][temp_location[1]] = 3
-
-        def movePlayerRight(self, player):
-	        temp_location = Player._get_location(player)
-	        playableGrid[temp_location[0]][temp_location[1]] = 0
-	        Player._move_right(player)
-	        playableGrid[temp_location[0]][temp_location[1]] = 3
+	#keep track of rooms crossed
 
 
-#player class
-class Player():
-#location is set by placement of "feet" of character
-    #takes up one space, variable is a coordinate array
-    #shouldn't automatically be 0,0; will be changed
-    def __init__(self, name):
-        self.name = name
-        self._col_num = 2
 
-        #list comprehension, python's equivalent to
-        # c's int location[2];
-        self.location = [0 for in range(self._col_num)]
+	#sends a json message to _________
+	#IS THIS RIGHT??
+	def update(self):
+		data = {'game_over': gameOver, 'game_time': self.gameTime, 'rooms_crossed': self.roomsCrossed}
+		json.dumps(data)
+		return
 
-    #used to set initial player location in a map
-    def _set_location(self, row, col):
-        self.location[0] = row
-        self.location[1] = col
-
-    #returns current location
-
-    def _get_location(self):
-        return self.location
-
-    #functions for single-space movement
-    def _move_up(self):
-        self.location[1] =  self.location[1] + 1
-
-    def _move_down(self):
-        self.location[1] = self.location[1] - 1
-
-    def _move_left(self):
-        self.location[0] = self.location[0] - 1
-
-    def _move_right(self):
-        self.location[0] - self.location[0] + 1
-
- 
-
-#obstacle class
-
-Class Obstacle():
-
-#location is set by placement of "base" of obstacle
-    #takes up one space, variable is a coordinate array
-    #shouldn't automatically be 0,0; will be changed
-    def __init__(self, name):
-        self.name = name
-        self._col_num = 2
-
-        #list comprehension, python's equivalent to
-        # c's  int location[2];
-        self.location = [0 for in range(self._col_num)]
-
-    #used to set initial player location in a map
-    def _set_location(self, row, col):
-        self.location[0] = row
-        self.location[1] = col
-
-    #returns current location
-    def _get_location(self):
-        return self.location
-
-    #functions for single-space movement
-
-    #do obstacles need movement functions?
-    def _move_up(self):
-        self.location[1] =  self.location[1] + 1
-
-    def _move_down(self):
-        self.location[1] = self.location[1] - 1
-
-    def _move_left(self):
-        self.location[0] = self.location[0] - 1
-
-    def _move_right(self):
-        self.location[0] - self.location[0] + 1
-
- 
+	#recieves a json message from ______
+	def get_change_request(self, m)
+		req = json.loads(m)
+		#do something here..
+		return
