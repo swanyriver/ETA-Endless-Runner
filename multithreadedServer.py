4e52d9c2bbc144c3ba4896270647c180acd42a52
Megan Fanning <megantfanning@gmail.com>
2016-07-26 17:03:21 -0700

    ready for integration

--- multithreadedServer.py
+++ multithreadedServer.py
@@ -1,53 +1,93 @@
-#!/usr/bin/python2.7
 import socket
 import threading
-from SocketServer import ThreadingTCPServer
-from SocketServer import BaseRequestHandler
-
-class ThreadedTCPRequestHandler(BaseRequestHandler):
-
+import thread
+import SocketServer
+###
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         serverActive=True
         # self.request is the TCP socket connected to the client
+        self.request.setblocking(0)
+
         while serverActive:
-            received=self.request.recv(1024)
-            #if nothing received close server
+            received=""
+            try:
+                received=self.request.recv(1024) 
+                #note:recv'd doesn't have to be 1024
+            except:
+                pass
+            
             if received==0:
                 serverActive=False
-                print "nothing received end server" #DEBUG only
-            else:
-                print "in server"#DEBUG only
-                data = self.request.recv(1024)
-                print data
-                cur_thread = threading.current_thread() #?TODO
-                response = "{}: {}".format(cur_thread.name, data)#?TODO
+
+            elif received !="":
+                cur_thread = threading.current_thread()
+                response = "{}: {}".format(cur_thread.name, received)
+                #print client response
+                print response
+                #send response
                 self.request.sendall(response)
-                # just send back the same data, but upper-cased
-                self.request.sendall(self.data.upper()+"\n")
-                #TODO call function to process user movement
-                #TODO call function to pass user text back to other client
+
+                # if received == up/down/left/right
+                if received=="w" or received =="a" or received=="s" or received=="d":
+                    lock.acquire()
+
+                    #check if client is vertical or horizontal
+                    if cur_thread.name=="Thread-2":
+                        #allow horizontal movement
+                        if received =="a" or received=="d":
+                            updatedState = game.get_change_request(data)
+                        else
+                            print "You control horizontal movement only!"
+
+                    elif cur_thread.name=="Thread-3":
+                        if received =="w" or received=="s":
+                            updatedState = game.get_change_request(data)
+                        else
+                            print "You control vertical movement only!"
+
+                    else:
+                        print "Too many players, please wait"
+                                    
+                    lock.release()
+                #-- End of locked thread --#
+
+                else:
+                    continue
+                ## send client movement    
+                print "sent to client: " + updatedState
+                self.request.sendall(updatedState + "\n")
+                    
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
 if __name__ == "__main__":
-    # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 0
+    # Port 0 selects an arbitrary unused port
+    HOST, PORT = "localhost", 9998
+    ##create lock object
+    lock = threading.Lock()
+    #global variables/objects must be created in main prior to threading
+    global count    
 
-    server = ThreadingTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+     #-- client code --# 
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     ip, port = server.server_address
+
+    #print port so client can connect
+    print port
 
     # Start a thread with the server -- that thread will then start one
     # more thread for each request
-    server_thread = threading.Thread()#target=server.serve_forever) <change serve forever to allow clients to terminate
+    server_thread = threading.Thread(target=server.serve_forever)
     # Exit the server thread when the main thread terminates
-   
-    #server_thread.daemon = True ####<<TODO remove
+    server_thread.daemon = True
     server_thread.start()
     print "Server loop running in thread:", server_thread.name
-    print "server port:",port
-    print "server host:",ip
-    while 1:
+
+    live=True;
+    while live:
         continue
 
-    #if()<<TODO replace while with this
     server.shutdown()
     server.server_close()
-    print "server is now closed"
