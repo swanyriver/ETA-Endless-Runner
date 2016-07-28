b40037813963796083874ba7c49f0873624d15f2
Brandon Swanson <swanyriver@gmail.com>
2016-07-27 17:58:02 -0700

    revert to integration

--- multithreadedserver.py
+++ multithreadedserver.py
@@ -9,32 +9,11 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.request.setblocking(0)
 
-        cur_thread = threading.current_thread()
-        myMessageQue = []
-
-
-        #send fist game room/map to client and add que to directory
-        lock.acquire()
-        # if not all actions have been taken assign one set to this thread
-        if availableActionSets:
-            allowedActionsForThreads[cur_thread.ident] = availableActionSets.pop()
+        #send fist game room/map to client
         self.request.sendall(game.get_update()+ "\n")
-        threadOutgoingMessages[cur_thread.ident] = myMessageQue
-        lock.release()
+
 
         while serverActive:
-
-            if myMessageQue:
-                lock.acquire()
-                # copy refrence to strings to be sent and empty que in dictionary
-                messagesToSend = list(myMessageQue)
-                del myMessageQue[:]
-                lock.release()
-
-                for msg in messagesToSend:
-                    self.request.sendall(msg + "\n")
-
-
             received=""
             try:
                 received=self.request.recv(1024)
@@ -47,6 +26,7 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
                 serverActive=False
 
             elif received !="":
+                cur_thread = threading.current_thread()
                 #use repr to show whitespace explicitly
                 response = "{}: {}".format(cur_thread.name, repr(received))
                 #print client response
@@ -56,17 +36,15 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
                     # todo relay chat message
                     print "(NETWORK) chat received: ", repr()
 
-                elif cur_thread.ident in allowedActionsForThreads and \
-                                received in allowedActionsForThreads[cur_thread.ident]:
+                elif received in allowedMovements[cur_thread.name]:
                     lock.acquire()
                     updatedState = game.get_change_request(received)
-                    for threadIdent, theirmessageQue in threadOutgoingMessages.items():
-                        if threadIdent != cur_thread.ident: theirmessageQue.append(updatedState)
                     lock.release()
 
                 #-- End of locked thread --#
 
                     ## send client movement
+                    #todo send to other client
                     print "sent to client: " + updatedState
                     self.request.sendall(updatedState + "\n")
 
@@ -80,7 +58,7 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
 if __name__ == "__main__":
     # Port 0 selects an arbitrary unused port
-    HOST, PORT = "localhost", 9999
+    HOST, PORT = "localhost", 9997
     ##create lock object
     lock = threading.Lock()
     #global variables/objects must be created in main prior to threading
@@ -96,14 +74,10 @@ if __name__ == "__main__":
     #create a new game state
     game = game_state.Gamestate()
 
-    allowedActionsForThreads = {}
-
-    availableActionSets = [
-        [networkKeys.ACTIONS.left, networkKeys.ACTIONS.right],
-        [networkKeys.ACTIONS.up, networkKeys.ACTIONS.down],
-    ]
-
-    threadOutgoingMessages = {}
+    allowedMovements = {
+        "Thread-2": [networkKeys.ACTIONS.left, networkKeys.ACTIONS.right],
+        "Thread-3": [networkKeys.ACTIONS.up, networkKeys.ACTIONS.down],
+    }
 
     # Start a thread with the server -- that thread will then start one
     # more thread for each request
