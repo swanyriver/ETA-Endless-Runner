ecbd4187ca5782437407078c319195841b780091
Brandon Swanson <swanyriver@gmail.com>
2016-08-10 19:41:47 -0700

    single player accepts port

--- singlePlayer.sh
+++ singlePlayer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python multithreadedserver.py -s 1>/dev/null 2>/dev/null &
+python multithreadedserver.py $1 -s 1>/dev/null 2>/dev/null &
 PID=$!
-python client.py 127.0.0.1 9999 2>/dev/null; 
+python client.py 127.0.0.1 $1 2>/dev/null; 
 kill $PID;
 
