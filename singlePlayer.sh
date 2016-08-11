8c8ff6747bed13e97332f06f8c13f510d854b8ca
Brandon Swanson <swanyriver@gmail.com>
2016-08-10 22:58:18 -0700

    Clean up single player launching script

--- singlePlayer.sh
+++ singlePlayer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+#stop other instances of our game servers from running on that port
+#waits for port to be freed but only if port was in use
+fuser ${1}/tcp -k > /dev/null 2>&1 && sleep .5
+
+#Launch server with supplied port and flag for single player mode
+#Launch in background and redirect output, allowing curses to run in same terminal
 python multithreadedserver.py $1 -s 1>/dev/null 2>/dev/null &
 PID=$!
-python client.py 127.0.0.1 $1 2>/dev/null; 
-kill $PID;
+
+#launch connected client
+python client.py 127.0.0.1 $1 2>/dev/null;
+
+#Terminate single use server 
+kill $PID > /dev/null 2>&1;
 
